8539b8e7665a29ce01ed20790825b71e5cc8b92e
pureblacker <78307022+pureblacker@users.noreply.github.com>
2021-01-31 21:57:04 +0800

    C#实现
    有简单界面

---
+++ WordFrequent/Form1.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace WordFrequent
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(40, 93);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(542, 28);
+            this.textBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button1.BackgroundImage")));
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button1.Location = new System.Drawing.Point(614, 93);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(42, 28);
+            this.button1.TabIndex = 1;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.upfile_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(37, 58);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(206, 18);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "上传文件所在文件夹路径";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(40, 223);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(542, 28);
+            this.textBox2.TabIndex = 3;
+            // 
+            // button2
+            // 
+            this.button2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button2.BackgroundImage")));
+            this.button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button2.Location = new System.Drawing.Point(614, 223);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(42, 28);
+            this.button2.TabIndex = 4;
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(40, 199);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(188, 18);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "选择结果文件保存路径";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(40, 310);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(149, 36);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "统计一元汉字";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(273, 310);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(149, 36);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "统计二元汉字组";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(497, 310);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(159, 36);
+            this.button5.TabIndex = 8;
+            this.button5.Text = "统计三元汉字组";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+    }
+}
+
---
+++ WordFrequent/Form1.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace WordFrequent
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void upfile_Click(object sender, EventArgs e)
+        {
+            /*打开文件夹*/
+            FolderBrowserDialog folderDialog = new FolderBrowserDialog();
+            if (folderDialog.ShowDialog() == DialogResult.OK)
+            { 
+                textBox1.Text = folderDialog.SelectedPath;
+            }
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            /*设置保存路径*/
+            OpenFileDialog fileDialog = new OpenFileDialog();
+            fileDialog.Multiselect = false;//是否同时选中多个文件
+            fileDialog.Title = "请选择文件路径";
+            fileDialog.Filter = "所有文件(*.*)|*.*";//获取文件路径，所有文件类型
+            //fileDialog.Filter = "文本文件 (*.txt)|*.txt";//过滤你想设置的文本文件类型（这是txt型）
+            if (fileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string file = fileDialog.FileName;
+                //MessageBox.Show("已选择文件:" + file, "选择文件提示", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                textBox2.Text = file;
+            }
+        }
+
+       
+
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            /*统计一元字频*/
+            string path;
+            path = textBox1.Text; // 文件夹目录。
+            DirectoryInfo dir = new DirectoryInfo(path);
+            FileInfo[] files = dir.GetFiles(); // 获取所有文件信息
+            Dictionary<char, int> word_dict = new Dictionary<char, int>();//字典用于保存结果及查找词
+            foreach (FileInfo items in files)//循环每个文件
+            {
+                StreamReader file = new StreamReader(items.FullName,true);//读取文件内容，false 不保留原来内容
+                string line;
+                while ((line = file.ReadLine()) != null)//按行读取，循环每一行
+                {
+                    line= line.Replace(" ", "");//去除空格
+                    foreach (char i in line) 
+                    {
+                        if (word_dict.ContainsKey(i))//word_dict.Keys.Contains()
+                            word_dict[i] += 1;//在字典出现过的词加一
+                        else
+                            word_dict[i] = 1;//未出现过的让其键等于一
+                    }
+                }
+                file.Close();
+            }
+            //排序，value.Value 按值排序，desending降序
+            var result = from value in word_dict orderby value.Value descending select value;
+            //写入文件
+            FileStream fs = new FileStream(textBox2.Text, FileMode.Create);
+            StreamWriter sw = new StreamWriter(fs);
+            foreach (KeyValuePair<char, int> kvp in result)
+            {
+                //开始写入
+                sw.WriteLine("key=" + kvp.Key + ",value=" + kvp.Value);
+            }
+            //清空缓冲区
+            sw.Flush();
+            //关闭流
+            sw.Close();
+            fs.Close();
+            MessageBox.Show("done!");
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            /*统计二元字频*/
+            string path=textBox1.Text; // 文件夹目录。
+            DirectoryInfo dir = new DirectoryInfo(path);
+            FileInfo[] files = dir.GetFiles(); // 获取所有文件信息
+            Dictionary<string, int> word_dict = new Dictionary<string, int>();//字典用于保存结果
+            string line;
+            string p="";
+            foreach (FileInfo item in files)
+            {
+                StreamReader file = new StreamReader(item.FullName, true);//false 不保留原来内容
+                while ((line = file.ReadLine()) != null)
+                {
+                    line = line.Replace(" ", "");
+                    foreach(char word in line)
+                    {
+                        p += word;
+                        if (p.Length == 2)
+                        {
+                            if (word_dict.ContainsKey(p))
+                                word_dict[p] += 1;
+                            else
+                                word_dict[p] = 1;
+                            p = p.Substring(1);//Substring(Int32)  子字符串在指定的字符位置开始并一直到该字符串的末尾。
+                                               //Substring(Int32, Int32)   子字符串从指定的字符位置开始且具有指定的长度。
+                                               //参数一：起始位置(从0开始);   参数二：指定长度
+                        }
+                    }  
+                }
+                file.Close();
+            }
+            //排序
+            var result = from value in word_dict orderby value.Value descending select value;
+            //写入文件
+            FileStream fs = new FileStream(textBox2.Text, FileMode.Create);
+            StreamWriter sw = new StreamWriter(fs);
+            foreach (KeyValuePair<string, int> kvp in result)
+            {
+                //开始写入
+                sw.WriteLine("key=" + kvp.Key + ",value=" + kvp.Value);
+            }
+            //清空缓冲区
+            sw.Flush();
+            //关闭流
+            sw.Close();
+            fs.Close();
+            MessageBox.Show("done!");
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            string path = textBox1.Text; // 文件夹目录。
+            DirectoryInfo dir = new DirectoryInfo(path);
+            FileInfo[] files = dir.GetFiles(); // 获取所有文件信息
+            Dictionary<string, int> word_dict = new Dictionary<string, int>();//字典用于保存结果
+            string line;
+            string p = "";
+            foreach (FileInfo item in files)
+            {
+                StreamReader file = new StreamReader(item.FullName, true);//false 不保留原来内容
+                while ((line = file.ReadLine()) != null)
+                {
+                    line = line.Replace(" ", "");
+                    foreach (char word in line)
+                    {
+                        p += word;
+                        if (p.Length == 3)
+                        {
+                            if (word_dict.ContainsKey(p))
+                                word_dict[p] += 1;
+                            else
+                                word_dict[p] = 1;
+                            p = p.Substring(1);
+                        }
+                    }
+                }
+                file.Close();
+            }
+            //排序
+            var result = from value in word_dict orderby value.Value descending select value;
+            //写入文件
+            FileStream fs = new FileStream(textBox2.Text, FileMode.Create);
+            StreamWriter sw = new StreamWriter(fs);
+            foreach (KeyValuePair<string, int> kvp in result)
+            {
+                //开始写入
+                sw.WriteLine("key=" + kvp.Key + ",value=" + kvp.Value);
+            }
+            //清空缓冲区
+            sw.Flush();
+            //关闭流
+            sw.Close();
+            fs.Close();
+            MessageBox.Show("done!");
+        }
+    }
+}
